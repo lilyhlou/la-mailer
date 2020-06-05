18816740b89481d50ed6298a46d76ad8133c3922
Nanak Nihal Khalsa <nkhalsa@vassar.edu>
2020-06-05 10:14:37 -0700

    initial commit -- tried to add activism-mail-bot template option

--- src/emails/email-builder.js
+++ src/emails/email-builder.js
@@ -1,14 +1,17 @@
+import ActivismMailBot from "./activism-mail-bot"
 import PoliceBrutalityLa from "./police-brutality-la"
 import BreonnaTaylor from "./breonna-taylor"
 import JackieLacy from "./jackie-lacy"
 
 const emailIdMap = {
+  "activism-mail-bot": ActivismMailBot,
   "police-brutality-la": PoliceBrutalityLa,
   "breonna-taylor": BreonnaTaylor,
   "jackie-lacy": JackieLacy,
 }
 
 export const emailIdTitleMap = {
+  "activism-mail-bot": "Automatically generated custom letter via https://github.com/alandgton/activism-mail-bot",
   "police-brutality-la": "Defund LAPD Template",
   "breonna-taylor": "Justice for Breonna Taylor Template",
   "jackie-lacy": "LA - Rescind Jackie Lacey Endorsement Template",
